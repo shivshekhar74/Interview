32eab9d2f38736be9b5d54ea7170ecd73576d7fb
Shiv <shivshekhar74@gmail.com>
2022-08-30 16:11:16 +0530

    Listing completed

--- app/src/main/java/com/intermind/interview/MainActivity.java
+++ app/src/main/java/com/intermind/interview/MainActivity.java
@@ -32,21 +32,25 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        ///** RecyclerView  ***///
         recyclerView = (RecyclerView) findViewById(R.id.recyclerview);
         recyclerView.setHasFixedSize(true);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
 
-        List<String> ascending_list = Get_ALL_Information();
-        Collections.sort(ascending_list);
-        for (String myStr: ascending_list) {
-            System.out.print(" " + myStr);
-        }
 
-        adapter = new My_Custom_Adapter(this,ascending_list);
+
+        ///*** GET PACKAGE NAME AND APP NAME ***///
+        List<String> app_name_ascending_list = App_name_list();
+        Collections.sort(app_name_ascending_list);
+        List<String> ascending_all_info = Get_ALL_Information();
+        Collections.sort(ascending_all_info);
+
+        ///*** Set Custom Adapter***//
+        adapter = new My_Custom_Adapter(this,ascending_all_info,app_name_ascending_list);
         recyclerView.setAdapter(adapter);
 
 
-        ///** Filter by data***///
+        ///** Filter by app name ***///
         search_name = (EditText) findViewById(R.id.search_name);
         search_name.addTextChangedListener(new TextWatcher() {
             @Override
@@ -68,32 +72,32 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    ///*** filter by app name****///
     private void filter(String text) {
         ArrayList<String> filter_data = new ArrayList<>();
-        for (String s : Get_ALL_Information()) {
+        List<String> app_name_ascending_list = App_name_list();
+        Collections.sort(app_name_ascending_list);
+        for (String s : app_name_ascending_list) {
             if (s.toLowerCase().contains(text.toLowerCase())) {
                 filter_data.add(s);
             }
         }
         adapter.filterList(filter_data);
     }
+
+
+    ///*** get all package name ***///
     public List<String> Get_ALL_Information(){
 
         List<String> ApkPackageName = new ArrayList<>();
-        List<String> app_name = new ArrayList<>();
-
         Intent intent = new Intent(Intent.ACTION_MAIN,null);
-
         intent.addCategory(Intent.CATEGORY_LAUNCHER);
-
         intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_RESET_TASK_IF_NEEDED );
-
         List<ResolveInfo> resolveInfoList = getPackageManager().queryIntentActivities(intent,0);
         for(ResolveInfo resolveInfo : resolveInfoList){
             ActivityInfo activityInfo = resolveInfo.activityInfo;
             if(!isSystemPackage(resolveInfo)){
                 ApkPackageName.add(activityInfo.applicationInfo.packageName);
-                app_name.add(GetAppName(activityInfo.applicationInfo.packageName));
             }
         }
 
@@ -105,6 +109,25 @@ public class MainActivity extends AppCompatActivity {
         return ((resolveInfo.activityInfo.applicationInfo.flags & ApplicationInfo.FLAG_SYSTEM) != 0);
     }
 
+    ///*** Add app name for filter****///
+    public List<String> App_name_list(){
+
+        List<String> app_name = new ArrayList<>();
+        Intent intent = new Intent(Intent.ACTION_MAIN,null);
+        intent.addCategory(Intent.CATEGORY_LAUNCHER);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_RESET_TASK_IF_NEEDED );
+        List<ResolveInfo> resolveInfoList = getPackageManager().queryIntentActivities(intent,0);
+        for(ResolveInfo resolveInfo : resolveInfoList){
+            ActivityInfo activityInfo = resolveInfo.activityInfo;
+            if(!isSystemPackage(resolveInfo)){
+                app_name.add(GetAppName(activityInfo.applicationInfo.packageName));
+            }
+        }
+        return app_name;
+
+    }
+
+    ///*** Get App name***///
     public String GetAppName(String ApkPackageName) {
 
         String Name = "";
@@ -128,4 +151,9 @@ public class MainActivity extends AppCompatActivity {
         }
         return Name;
     }
+
+
+
+
+
 }
--- app/src/main/java/com/intermind/interview/My_Custom_Adapter.java
+++ app/src/main/java/com/intermind/interview/My_Custom_Adapter.java
@@ -2,7 +2,9 @@ package com.intermind.interview;
 
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.ActivityInfo;
 import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.graphics.drawable.Drawable;
 import android.view.LayoutInflater;
@@ -22,11 +24,13 @@ import java.util.List;
 
 public class My_Custom_Adapter extends RecyclerView.Adapter<My_Custom_Adapter.ViewHolder> {
 
-    private List<String> data;
+    private List<String> data, app_name;
     private Context context;
+    ArrayList<String> add_package = new ArrayList<>();
 
-    public My_Custom_Adapter(Context context, List<String> data) {
+    public My_Custom_Adapter(Context context, List<String> data, List<String> app_name) {
         this.data = data;
+        this.app_name = app_name;
         this.context = context;
     }
 
@@ -40,23 +44,53 @@ public class My_Custom_Adapter extends RecyclerView.Adapter<My_Custom_Adapter.Vi
     @Override
     public void onBindViewHolder(ViewHolder holder, int position) {
 
-        holder.txt_app_name.setText(GetAppName(data.get(position)));
-        final String ApplicationPackageName = (String) data.get(position);
-        holder.txt_package_name.setText(ApplicationPackageName);
-        holder.ima_app.setImageDrawable(getAppIconByPackageName(data.get(position)));
+        /// *** Set App Name***///
+        holder.txt_app_name.setText(app_name.get(position));
 
+        /// *** Set all Parameter  ***///
+        for (int i = 0; i < data.size(); i++) {
+            if (app_name.get(position).equals(GetAppName(data.get(i)))) {
+                String ApplicationPackageName = (String) data.get(i);
+
+                add_package.add(position, ApplicationPackageName);
+                holder.txt_package_name.setText(ApplicationPackageName);
+                holder.ima_app.setImageDrawable(getAppIconByPackageName(data.get(i)));
+
+                try {
+
+                    PackageInfo pkg_info = context.getPackageManager().getPackageInfo(ApplicationPackageName, 0);
+                    String version_name = pkg_info.versionName;
+                    int version_code = pkg_info.versionCode;
+                    String className = pkg_info.applicationInfo.className;
+                    holder.txt_version_name.setText("Version Name - " + version_name);
+                    holder.txt_version_code.setText("Version Code - " + String.valueOf(version_code));
+                    holder.txt_class_name.setText("Class Name - " + className);
+
+                } catch (PackageManager.NameNotFoundException e) {
+
+                    e.printStackTrace();
+                }
+
+
+                break;
+
+            }
+
+
+        }
+
+        ///***** click item***////
         holder.item_click.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
-                Intent intent = context.getPackageManager().getLaunchIntentForPackage(ApplicationPackageName);
+                Intent intent = context.getPackageManager().getLaunchIntentForPackage(add_package.get(position));
                 if (intent != null) {
-
                     context.startActivity(intent);
 
                 } else {
 
-                    Toast.makeText(context, ApplicationPackageName + " Error, Please Try Again.", Toast.LENGTH_LONG).show();
+                    Toast.makeText(context, add_package.get(position) + " Error, Please Try Again.", Toast.LENGTH_LONG).show();
                 }
             }
         });
@@ -65,12 +99,12 @@ public class My_Custom_Adapter extends RecyclerView.Adapter<My_Custom_Adapter.Vi
 
     @Override
     public int getItemCount() {
-        return data.size();
+        return app_name.size();
     }
 
     class ViewHolder extends RecyclerView.ViewHolder {
 
-        TextView txt_app_name, txt_package_name;
+        TextView txt_app_name, txt_package_name, txt_class_name, txt_version_name, txt_version_code;
         ImageView ima_app;
         LinearLayout item_click;
 
@@ -78,12 +112,16 @@ public class My_Custom_Adapter extends RecyclerView.Adapter<My_Custom_Adapter.Vi
             super(itemView);
             txt_app_name = (TextView) itemView.findViewById(R.id.txt_app_name);
             txt_package_name = (TextView) itemView.findViewById(R.id.txt_package_name);
+            txt_class_name = (TextView) itemView.findViewById(R.id.txt_class_name);
+            txt_version_code = (TextView) itemView.findViewById(R.id.txt_version_code);
+            txt_version_name = (TextView) itemView.findViewById(R.id.txt_version_name);
             ima_app = (ImageView) itemView.findViewById(R.id.ima_app);
             item_click = (LinearLayout) itemView.findViewById(R.id.item_click);
         }
     }
 
 
+    ///**** Set app icon ****///
     public Drawable getAppIconByPackageName(String ApkTempPackageName) {
 
         Drawable drawable;
@@ -100,6 +138,7 @@ public class My_Custom_Adapter extends RecyclerView.Adapter<My_Custom_Adapter.Vi
         return drawable;
     }
 
+    ///**** Set app name ****///
     public String GetAppName(String ApkPackageName) {
 
         String Name = "";
@@ -124,9 +163,9 @@ public class My_Custom_Adapter extends RecyclerView.Adapter<My_Custom_Adapter.Vi
         return Name;
     }
 
-
-    public void filterList(ArrayList<String> filterdNames) {
-        this.data = filterdNames;
+    public void filterList(ArrayList<String> fiter_app_name) {
+        this.app_name = fiter_app_name;
         notifyDataSetChanged();
     }
+
 }
